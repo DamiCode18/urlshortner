be7ad1654bfa1b3c9e26fd1c6432ccad68564001
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2021-04-21 22:47:07 +0100

    feat/navbar responsiveness fixed

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -2,16 +2,18 @@ import Link from 'next/link';
 import { Component } from 'react';
 import navStyle from '../../styles/Nav.module.css';
 
+
+
 export default class Navbar extends Component {
-	// hambuger=()=>{
-	// 	var x = document.getElementById("myLinks");
-	// 	if (x.style.display === "block") {
-	// 		x.style.display = "none";
-	// 	} else {
-	// 		x.style.display = "block";
-	// 	}
-	// }
+	state = {
+		isExpanded: false
+	}
+	
+	hambuger=()=>{
+			this.setState({isExpanded: !this.state.isExpanded});
+	}
 	render() {
+		const {isExpanded} = this.state;
 		return (
 			<div className={navStyle.navBg}>
 				<div>
@@ -19,29 +21,56 @@ export default class Navbar extends Component {
 						<a className={navStyle.hm}>Urlshortner</a>
 					</Link>
 				</div>
+				<div>
 				<div className={navStyle.fa}>
-						<i class="fa fa-bars text-2xl"></i>
+						<i className="fa fa-bars text-2xl" onClick={this.hambuger}></i>
+						</div>
+						{isExpanded && window.innerWidth < 769 ?
+						<div className={navStyle.hbg}>
+						<div>
+							<ul className={navStyle.ul}>
+								<li className={navStyle.li}>
+									<Link href="#">About</Link>
+								</li>
+								<li className={navStyle.li}>
+									<Link href="#">Services</Link>
+								</li>
+								<li className={navStyle.li}>
+									<Link href="#">Offers</Link>
+								</li>
+							</ul>					
+						</div>
+						<div>
+							<Link href="#">
+								<a className={navStyle.btn}>Login</a>
+							</Link>
+						</div>
+						</div> : 
+							null
+			
+	}
+						<div className={navStyle.hide}>
+							<div>
+								<ul className={navStyle.ul}>
+									<li className={navStyle.li}>
+										<Link href="#">About</Link>
+									</li>
+									<li className={navStyle.li}>
+										<Link href="#">Services</Link>
+									</li>
+									<li className={navStyle.li}>
+										<Link href="#">Offers</Link>
+									</li>
+								</ul>					
+							</div>
+							<div>
+								<Link href="#">
+									<a className={navStyle.btn}>Login</a>
+								</Link>
+							</div>
+							</div>
 					</div>
-				<span id="myLinks" class={navStyle.hide}>
-				<div>
-					<ul className={navStyle.ul}>
-						<li className={navStyle.li}>
-							<Link href="#">About</Link>
-						</li>
-						<li className={navStyle.li}>
-							<Link href="/#">Services</Link>
-						</li>
-						<li className={navStyle.li}>
-							<Link href="#">Offers</Link>
-						</li>
-					</ul>					
-				</div>
-				<div>
-					<Link href="#">
-						<a className={navStyle.btn}>Login</a>
-					</Link>
-				</div>
-				</span>
+					
 			
 			</div>
 		);
